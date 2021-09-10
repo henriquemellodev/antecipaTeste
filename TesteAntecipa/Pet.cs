380d2ea9c849f1090e9a4ef5c61d9b453323caf9
henriquemellodev <henriquemellodev@gmail.com>
2021-09-10 17:40:42 -0300

    Primeiro Commit

---
+++ TesteAntecipa/Pet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TesteAntecipa
+{
+    public class Pet
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("category")]
+        public Category Category { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("photoUrls")]
+        public List<string> PhotoUrls { get; set; }
+
+        [JsonPropertyName("tags")]
+        public List<Tag> Tags { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonPropertyName("id")]
+        public Int64 Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    public class Tag
+    {
+        [JsonPropertyName("id")]
+        public Int64 Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ TesteAntecipa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace TesteAntecipa
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            // Capturar o valor do Status: available, pending, sold
+            string status = Console.ReadLine().Trim();
+
+            // Endpoint
+            client.BaseAddress = new Uri("https://petstore.swagger.io/v2/pet/findByStatus?status=available");
+            
+            // Limpar Cabeçalho
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            // Adicionar o valor no Cabeçalho
+            client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Requisição GET
+            var request = client.GetStreamAsync("?status=" + status);
+
+            // Deserialização do JSON, transformando na lista do Objeto PET
+            var listPet = await JsonSerializer.DeserializeAsync<List<Pet>>(await request);
+
+            // Imprimir Requisição ID + Nome
+
+            foreach (var print in listPet)
+                Console.WriteLine("ID: " + print.Id + " Name: " + print.Name);
+        }
+    }
+}
